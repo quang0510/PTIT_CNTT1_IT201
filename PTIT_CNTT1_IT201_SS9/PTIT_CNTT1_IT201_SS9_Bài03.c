b43ebe938549c690477a93a452f43859694cfe57
quang0510 <nguyendangquang0510@gmail.com>
2025-07-03 07:16:44 +0700

    Add files via upload

--- PTIT_CNTT1_IT201_SS9/PTIT_CNTT1_IT201_SS9_Bài03.c
+++ PTIT_CNTT1_IT201_SS9/PTIT_CNTT1_IT201_SS9_Bài03.c
@@ -1,55 +1,63 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-struct Node {
+typedef struct Node {
+
     int data;
-    struct Node *next;
-};
+    struct Node* next;
+
+}Node;
+
+Node* createNode(int data) {
+
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode -> data = data;
+    newNode -> next = NULL;
+    return newNode;
+
+}
+
+bool search(Node* head, int target) {
+
+    Node* current = head;
+    int index = -1;
+
+    while (current != NULL) {
+
+        if (current -> data == target) {
+            return true;
+        }
+        current = current -> next;
+
+    }
+    return false;
+}
+
 
 int main() {
-    int value = 0;
-    scanf("%d", &value);
-    struct Node *node1 = NULL;
-    struct Node *node2 = NULL;
-    struct Node *node3 = NULL;
-    struct Node *node4 = NULL;
-    struct Node *node5 = NULL;
-    node1 = (struct Node *) malloc(sizeof(struct Node));
-    node2 = (struct Node *) malloc(sizeof(struct Node));
-    node3 = (struct Node *) malloc(sizeof(struct Node));
-    node4 = (struct Node *) malloc(sizeof(struct Node));
-    node5 = (struct Node *) malloc(sizeof(struct Node));
-    node1->data = 1;
-    node1->next = node2;
-    node2->data = 2;
-    node2->next = node3;
-    node3->data = 3;
-    node3->next = node4;
-    node4->data = 4;
-    node4->next = node5;
-    node5->data = 5;
-    node5->next = NULL;
+    Node* head = NULL;
+    Node* node1 = createNode(1);
+    Node* node2 = createNode(2);
+    Node* node3 = createNode(3);
+    Node* node4 = createNode(4);
+    Node* node5 = createNode(9);
+    Node* node6 = createNode(10);
 
-    int flag = 1;
+    head = node1;
+    node1 -> next = node2;
+    node2 -> next = node3;
+    node3 -> next = node4;
+    node4 -> next = node5;
+    node5 -> next = node6;
 
-    while (node1 != NULL) {
+    if (search(head, 7)) {
 
-        if (node1->data == value) {
-            printf("True");
-            return 0;
-        } else {
-            flag = 0;
-        }
-        node1 = node1->next;
-    }
-    if (!flag) {
-        printf("False");
+        printf("true\n");
+
+    }else {
+        printf("false\n");
     }
 
-    free(node1);
-    free(node2);
-    free(node3);
-    free(node4);
-    free(node5);
+
 }
